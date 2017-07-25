abda07647c05519bd9c4e9793828962d685b26d9
Dan King <dan.king@viewpoint.pro>
2017-07-25 12:44:59 -0700

    Moved 'fortune cookie' functionality into module.

--- index.js
+++ index.js
@@ -15,13 +15,8 @@ app.use(express.static(__dirname + '/public'));
 //app.use("/public", express.static(__dirname + '/public'));
 
 
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
+// Include fortune data
+var fortune = require('./lib/fortune.js');
 
 
 //
@@ -48,10 +43,12 @@ app.get('/', function(req, res) {
 
 // About
 app.get('/about', function(req, res) {
-	var randomFortune =
-		fortunes[Math.floor(Math.random() * fortunes.length)];
+	//var randomFortune =
+	//	fortunes[Math.floor(Math.random() * fortunes.length)];
 
-	res.render('about', { fortune: randomFortune });
+	//res.render('about', { fortune: randomFortune });
+
+	res.render('about', { fortune: fortune.getFortune() });
 });
 
 // Dingo
